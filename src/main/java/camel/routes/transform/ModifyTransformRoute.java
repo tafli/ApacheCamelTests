976f7b18726d967ec246ebbf5fd2b14d15ce8457
Andreas Boss <andi@theboss.ch>
2019-02-13 14:38:31 +0100

    Transform

---
+++ src/main/java/camel/routes/transform/ModifyTransformRoute.java
@@ -0,0 +1,14 @@
+package camel.routes.transform;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ModifyTransformRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("direct:transformInput")
+                .log("Received message BEFORE transform is ${body} and Headers are ${headers}")
+                .transform(body().regexReplaceAll(",","*"))
+                .log("Received message AFTER transform is ${body} and Headers are ${headers}")
+                .to("mock:output");
+    }
+}
